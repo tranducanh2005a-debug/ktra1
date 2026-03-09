4d21d88c487a9d1323c68af89013e9afa49c80bb
tranducanh2005a-debug <tranducanh2005a@gmail.com>
2026-03-09 11:09:16 +0700

    update

--- screens/ScanScreen.js
+++ screens/ScanScreen.js
@@ -10,11 +10,37 @@ export default function ScanScreen({ navigation }) {
         <Text style={styles.back}>←</Text>
       </TouchableOpacity>
 
-      {/* Product */}
-      <Image
-        source={require("../image/glass-bottle-mockups-for-food-and-beverage-packaging-cover 1.png")}
-        style={styles.image}
-      />
+      {/* Scan Area */}
+      <View style={styles.scanArea}>
+
+        {/* Product */}
+        <Image
+          source={require("../image/glass-bottle-mockups-for-food-and-beverage-packaging-cover 1.png")}
+          style={styles.image}
+        />
+
+        {/* Scan Frame */}
+        <Image
+          source={require("../image/Vector 1.png")}
+          style={styles.topLeft}
+        />
+
+        <Image
+          source={require("../image/Vector 2.png")}
+          style={styles.topRight}
+        />
+
+        <Image
+          source={require("../image/Vector 3.png")}
+          style={styles.bottomLeft}
+        />
+
+        <Image
+          source={require("../image/Vector 4.png")}
+          style={styles.bottomRight}
+        />
+
+      </View>
 
       {/* Product info */}
       <View style={styles.bottomCard}>
@@ -41,6 +67,7 @@ export default function ScanScreen({ navigation }) {
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
     backgroundColor: "#E8D9C5",
@@ -52,11 +79,49 @@ const styles = StyleSheet.create({
     marginTop: 40
   },
 
+  scanArea: {
+    marginTop: 30,
+    height: 420,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
   image: {
-    width: "100%",
-    height: 450,
-    resizeMode: "contain",
-    marginTop: 30
+    width: "80%",
+    height: "100%",
+    resizeMode: "contain"
+  },
+
+  topLeft: {
+    position: "absolute",
+    top: 0,
+    left: 40,
+    width: 70,
+    height: 70
+  },
+
+  topRight: {
+    position: "absolute",
+    top: 0,
+    right: 40,
+    width: 70,
+    height: 70
+  },
+
+  bottomLeft: {
+    position: "absolute",
+    bottom: 0,
+    left: 40,
+    width: 70,
+    height: 70
+  },
+
+  bottomRight: {
+    position: "absolute",
+    bottom: 0,
+    right: 40,
+    width: 70,
+    height: 70
   },
 
   bottomCard: {
@@ -69,8 +134,8 @@ const styles = StyleSheet.create({
   },
 
   smallImg: {
-    width: 55,
-    height: 70,
+    width: 50,
+    height: 75,
     borderRadius: 10,
     marginRight: 10
   },
@@ -83,4 +148,5 @@ const styles = StyleSheet.create({
     alignItems: "center",
     borderRadius: 10
   }
+
 });
