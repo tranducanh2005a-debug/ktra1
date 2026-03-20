9a4fcaac825bb03abdad8c28e3721ff0302ca318
tranducanh2005a-debug <tranducanh2005a@gmail.com>
2026-03-20 14:54:19 +0700

    update UI tab bar

--- navigation/MainTab.js
+++ navigation/MainTab.js
@@ -15,18 +15,16 @@ export default function MainTab() {
         headerShown: false,
         tabBarShowLabel: false,
 
-        // 🔥 TAB STYLE ĐẸP HƠN
         tabBarStyle: {
           position: "absolute",
-          bottom: 15,
-          left: 15,
-          right: 15,
-          height: 75,
+          bottom: 10,
+          left: 20,
+          right: 20,
+          height: 100,
           borderRadius: 30,
           backgroundColor: "#fff",
           borderTopWidth: 0,
 
-          // shadow mượt
           elevation: 10,
           shadowColor: "#000",
           shadowOpacity: 0.08,
@@ -34,15 +32,27 @@ export default function MainTab() {
           shadowOffset: { width: 0, height: 8 }
         },
 
-        // 🔥 ICON
+        tabBarItemStyle: {
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center"
+        },
+
+        
+        tabBarIconStyle: {
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center"
+        },
+
         tabBarIcon: ({ focused }) => {
           let icon;
 
           if (route.name === "Home") {
             icon = require("../assets/Group 152.png");
-          } else if (route.name === "Scan") {
-            icon = require("../assets/Group 153.png");
           } else if (route.name === "Bag") {
+            icon = require("../assets/Group 153.png");
+          } else if (route.name === "Scan") {
             icon = require("../assets/Vector.png");
           } else if (route.name === "History") {
             icon = require("../assets/Group 154.png");
@@ -53,34 +63,39 @@ export default function MainTab() {
           return (
             <View
               style={{
-                width: 50,
-                height: 50,
+                flex: 1, 
                 justifyContent: "center",
-                alignItems: "center",
-
-                // active effect
-                backgroundColor: focused ? "#FDEDE7" : "transparent",
-                borderRadius: 15,
-                transform: [{ scale: focused ? 1.1 : 1 }] // phóng to nhẹ
+                alignItems: "center"
               }}
             >
-              <Image
-                source={icon}
+              <View
                 style={{
-                  width: 26,
-                  height: 26,
-                  resizeMode: "contain",
-                  tintColor: focused ? "#f28b54" : "#bbb"
+                  width: 45,
+                  height: 45,
+                  justifyContent: "center",
+                  alignItems: "center",
+                  borderRadius: 12,
+                  backgroundColor: focused ? "#FDEDE7" : "transparent"
                 }}
-              />
+              >
+                <Image
+                  source={icon}
+                  style={{
+                    width: 24,
+                    height: 24,
+                    resizeMode: "contain",
+                    tintColor: focused ? "#f28b54" : "#bbb"
+                  }}
+                />
+              </View>
             </View>
           );
         }
       })}
     >
       <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Scan" component={ScanScreen} />
       <Tab.Screen name="Bag" component={ScanScreen} />
+      <Tab.Screen name="Scan" component={ScanScreen} />
       <Tab.Screen name="History" component={ScanScreen} />
       <Tab.Screen name="Cart" component={CartScreen} />
     </Tab.Navigator>
